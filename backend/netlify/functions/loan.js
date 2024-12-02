3e9f6039700d45eb2ceaf2f13857e1ee520b03a3
Im-in123 <69424870+Im-in123@users.noreply.github.com>
2024-12-02 11:43:02 +0000

    changes

---
+++ backend/netlify/functions/loan.js
@@ -0,0 +1,25 @@
+// netlify/functions/loan.js
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+// Import the loan controller functions:
+const { requestLoan, getLoanHistory, getLoanDetails } = require('../../controllers/loanController');
+
+// Protect middleware (if you have authentication):
+const { protect } = require('../middleware/authMiddleware');
+
+app.post('/request', protect, requestLoan);
+app.get('/history', protect, getLoanHistory);
+app.get('/:loanId', protect, getLoanDetails);
+
+exports.handler = async (event, context) => {
+    return new Promise((resolve, reject) => {
+        app(event, context, (response) => {
+            resolve({
+                statusCode: 200,
+                body: JSON.stringify(response),
+            });
+        });
+    });
+};
