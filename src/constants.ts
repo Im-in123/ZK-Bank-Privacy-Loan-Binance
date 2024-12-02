b881d1c3ff84865e91255e4e9096b802bb60bbce
Im-in123 <69424870+Im-in123@users.noreply.github.com>
2024-12-02 17:01:49 +0000

    changes

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,4 @@
-//export const BASE_URL = "https://ideal-tribble-vw56rpqj6ggfvwx-5000.app.github.dev";
-export const BASE_URL = "https://zk-bank-privacy-loan-binance.vercel.app";
-
+export const BASE_URL = "https://ideal-tribble-vw56rpqj6ggfvwx-5000.app.github.dev";
 export const CONTRACT_ADDRESS = "0xf8B2Ec2c9bA0E473E3aE4682561229e0bCf274F5";
 export const APP_ID = "b7627e76-b9f2-41b0-b954-2bc5f63ecec3";
 export const SCHEMA_ID = "7b7b31ecbc654213ba7fc189b01d21f3";
