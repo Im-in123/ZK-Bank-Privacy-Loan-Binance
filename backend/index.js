901da3a783c6893a78e660dc993b19c6ebfe1af5
Im-in123 <69424870+Im-in123@users.noreply.github.com>
2024-12-02 16:53:09 +0000

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,26 @@
-// index.js
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+
 const authRoutes = require("./routes/authRoutes.js");
 const loanRoutes = require("./routes/loanRoutes.js");
 
-
- 
 dotenv.config();
-const app = express();
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/auth", authRoutes); // Use auth routes
-app.use("/loans", loanRoutes); // Use loan routes
+app.use("/auth", authRoutes);
+app.use("/loans", loanRoutes);
 app.get("/", (req, res) => {
-    res.json({
-      message: "Welcome to ZK-Bank Privacy Loan (Binance) API!",
-      availableEndpoints: ["/auth", "/loans"]
-    });
+  res.json({
+    message: "Welcome to ZK-Bank Privacy Loan (Binance) API!",
+    availableEndpoints: ["/auth", "/loans"],
   });
-  
-
-
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
- 
+
+
 module.exports = app;
